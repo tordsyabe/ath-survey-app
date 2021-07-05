eb390dde2939eba83cbc73845147b9600ca6ad02
John Llave <extordsllave@gmail.com>
2021-07-05 16:55:02 +0400

    change fetch api to jqeruy ajax

--- athsurveyapp/static/js/main.js
+++ athsurveyapp/static/js/main.js
@@ -7,46 +7,28 @@ $(document).ready(function(){
         var comp_id = event.target.value;
         var staffSelection =  $('#staff');
 
-        console.log(comp_id);
-        staffSelection.selectpicker('refresh');
-        
-        populateStaffSelection(comp_id).then(employees => {
-            staffSelection.selectpicker('refresh');     
-            options = ""
-            for (var i=0; i<employees.length; i++){
-                    options += `<option value="${employees[i].id}">${employees[i].name}</option>`;
+        console.log(comp_id);    
+
+        $.ajax({
+            type: "GET",
+            url: `/employees/${comp_id}`,
+            contentType: "application/json",
+            dataType: "json",
+            success: function(data) {
+                options = ""
+                for (var i=0; i<data.length; i++){
+                     options += `<option value="${data[i].id}">${data[i].name}</option>`;
+                }
+                staffSelection.empty().append(options);
+                staffSelection.selectpicker('refresh');
+
+            },
+            error: function(error) {
+                console.log(error);
             }
-        
-
-            staffSelection.empty().append(options);
-            staffSelection.selectpicker('refresh');
-            console.log(employees);
-        });
-
-        // $.ajax({
-        //     type: "GET",
-        //     url: `/employees/${comp_id}`,
-        //     contentType: "application/json",
-        //     dataType: "json",
-        //     success: function(data) {
-        //         options = ""
-        //         for (var i=0; i<data.length; i++){
-        //              options += `<option value="${data[i].id}">${data[i].name}</option>`;
-        //         }
-        //         staffSelection.selectpicker('refresh');
-        //         staffSelection.empty().append(options);                
-        //     },
-        //     error: function(error) {
-        //         console.log(error);
-        //     }
-        //   });
+          });
 
 
     })
 });
 
-async function populateStaffSelection(comp_id) {
-    const response = await fetch(`/employees/${comp_id}`);
-    const data = await response.json();
-    return data;
-}
